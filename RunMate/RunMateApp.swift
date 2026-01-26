c9d44e4635be29dcb825ccac62ea2fc6e017896a
gaozhongkui <gaozhongkui@meetstarinc.com>
2026-01-26 11:49:03 +0800

    init project

---
+++ RunMate/RunMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunMateApp.swift
+//  RunMate
+//
+//  Created by gaozhongkui on 2026/1/26.
+//
+
+import SwiftUI
+
+@main
+struct RunMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
