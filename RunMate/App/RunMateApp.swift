db23a79ad098db39708b71384a50ac56a190cfe0
gaozhongkui <gaozhongkui@meetstarinc.com>
2026-04-22 14:36:18 +0800

    新增：Firebase的策略配置和打点

--- RunMate/App/RunMateApp.swift
+++ RunMate/App/RunMateApp.swift
@@ -5,10 +5,23 @@
 //  Created by gaozhongkui on 2026/1/26.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct RunMateApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
